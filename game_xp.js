dd2b2247f40b1c8f01c345276ee835f260472a0e
gomox79 <81533646+gomox79@users.noreply.github.com>
2022-12-16 12:18:14 +0300

    Update game_xp.js

--- game_xp.js
+++ game_xp.js
@@ -1,391 +1,313 @@
+// Constants
+const height = 600;
+const width = 1400;
+const squareDim = 125;
+
 // Canvas element
-var canvas = document.getElementById("canvas");
-const context = document.querySelector("canvas").getContext("2d");
-context.canvas.height = 600;
-context.canvas.width = 1400;
+let canvas = document.getElementById('canvas');
+let context = document.querySelector('canvas').getContext('2d');
+context.canvas.height = height;
+context.canvas.width = width;
 
-// Chrono
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
-setInterval(setTime, 1000);
+// Main script variables
+var demerits = 0;
+var lvlToken = 0;
+var killCoeff = 1;
+var paused = false;
 
-function setTime() {
-  if (kill != 0 && paused == false) ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-}
+// HTML elements
+var dcount = document.getElementById('dem');
+var lvl = document.getElementById('off');
+var loc = document.getElementById('loc');
+var bg = document.getElementById('bg1');
+var img1 = document.getElementById('pic'); // Player
+var img2 = document.getElementById('danger'); // Dynamic obstacle
+var img3 = document.getElementById('vodka'); // Static obstacle
 
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
-  }
-}
+let bgMus = document.getElementById('myAudio'); // Background music
+let succMus = document.getElementById('itsAudio'); // Success music
+let failMus = document.getElementById('yourAudio'); // Failure music
+let jmpSound = document.getElementById('herAudio'); // Jump sound
+let colSound = document.getElementById('hisAudio'); // Collision sound
+
+// Player element
+let player = {
+	height: squareDim,
+	width: squareDim,
+	x: 0,
+	y: 0,
+	Vx: 1,
+	Vy: 0,
+	jumping: false,
+};
+
+// Dynamic obstacle element
+let opponent = {
+	height: squareDim,
+	width: squareDim,
+	x: 1390,
+	y: 586 - 16 - 125,
+	Vx: -1
+};
 
 // Initial conditions
 let frameCount = 1;
 let obCount = frameCount;
-var obCol = 100;
-const obXCoors = [];
-var Cols = [];
+let statXCoors = [];
+let statCols = [];
+dcount.value = 0;
+lvl.value = 'English';
+loc.value = 'ACME';
 
-// Variables
-var demerits = 0;
-var token = 0;
-var kill = 1;
-var paused = false;
+// Other variables
+//let statXCoor;
 
-//var perjum = 0.5;
-
-// Scret codes
-// cheat = localStorage.getItem('cheat');
-
-// Variable text / images
-var dcount = document.getElementById("dem"); dcount.value = 0;
-var lvl = document.getElementById("off"); lvl.value = "English";
-var loc = document.getElementById("loc"); loc.value = "ACME";
-var bg=document.getElementById("bg1");
-var pic = [];
-var pic2 = [];
-var pic3 = [];
-
-// Player / obstacle
-pic=document.getElementById("pic");
-pic2=document.getElementById("danger");
-pic3=document.getElementById("vodka");
-	
-// Audio
-var music = document.getElementById("myAudio");
-var sound = document.getElementById("yourAudio");
-var oof = document.getElementById("hisAudio");
-var boing = document.getElementById("herAudio");
-var yay = document.getElementById("itsAudio");
-
-const square = {
-
-  height: 125,
-  jumping: false,
-  coltok: true,
-  width: 125,
-  x: 0,
-  xVelocity: 1,
-  y: 0,
-  yVelocity: 0
-};
-
-const notsquare = {
-	
-	height: 125,
-	jumping: false,
-	width: 125,
-	x: 1390,
-	xVelocity: -1,
-	y: 586 - 16 - 125,
-	yVelocity: 0
-};
-
-// Obstacles
+// Static obstacles generation
 const nextFrame = () => {
-	frameCount++;
+	frameCount ++;
 	
-	for (let i = 0; i < obCount; i++) {
-		obXCoor = Math.floor(Math.random() * (1165 - 140 + 1) + 140);
-		obXCoors.push(obXCoor);
+	for (let i = 0; i < obCount; i ++) {
+		statXCoor = Math.floor(Math.random()*(1165 - 140 + 1) + 140);
+		statXCoors.push(statXCoor);
 	}
 }
 
-// Control
+// Player control
 const controller = {
 	left: false,
 	right: false,
 	up: false,
 	esc: false,
-	keyListener: function (event) {
-		var key_state = (event.type == "keydown") ? true : false;
-
+	keyListener: function(event) {
+		let keyState = (event.type == 'keydown')? true : false;
+		
 		switch (event.keyCode) {
-			case 38:
-			controller.up = key_state;
+			case 38: controller.up = keyState;
 			break;
-			case 27:
-			controller.esc = key_state;
+			case 27: controller.esc = keyState;
 			break;
+			default: break;
 		}
+	}
+}
+
+// Chronometer functions
+let secLabel = document.getElementById('seconds');
+let minLabel = document.getElementById('minutes');
+let timeCount = 0;
+if (kill && !paused) setInterval(setTime, 1000); // Calls setTime() each seconds
+
+function setTime() {
+	++ timeCount;
+	secLabel.innerHTML = pad(timeCount % 60);
+	minLabel.innerHTML = pad(timeCount / 60);
+}
+
+function pad(timeVal) {
+	let timeString = timeVal + '';
 	
-		if (controller.esc) {
-			menu();
-		}
+	if (timeString.length < 2) return '0' + timeString;
+	else return timeString;
+}
+
+// Collision function
+function collide() {
+	colSound.play();
+	player.x = -20;
+	player.y = 586 - 16 - 32;
+	demerits += 50;
+	lvlToken ++;
+	dcount.value = parseInt(dcount.value) + 10*lvlToken;
+	
+	switch (lvlToken) {
+		case 1: lvl.value = 'Sloppy';
+		break;
+		case 2: lvl.value = 'Irish';
+		break;
+		case 3: lvl.value = 'Spanish';
+		break;
+		case 4: lvl.value = 'Polish';
+		break;
+		case 5: 
+			lvl.value = 'Gibberish';
+			fail();
+		break;
+		default: lvl.value = 'Model Mid';
+		break;
 	}
+	
+	document.getElementById('field_0').style.display = 'block';
+	setTimeout(function() document.getElementById('field_0').style.display = 'none';, 1000);
+	loop();
 }
 
-// Game over
-function over() {
-	kill = 0;
-	document.getElementById('field_0').style.display='none';
-	document.getElementById('field_1').style.display='block';
-	music.pause();
-	sound.play();
-}
-
-// Menu
-function menu() {
-	music.pause();
-	paused = !paused;
-	if (paused == true) {
-		document.getElementById('field_2').style.display='block';
-	}
-	draw();
-}
-
-// Resume
-function resume() {
-	paused = !paused;
-	document.getElementById('field_2').style.display='none';
-	draw();
-}
-
-// New game
-function restart() {
-	location.reload()
-}
-
-// Success
+// Success function
 function success() {
-	kill = 0;
-	document.getElementById('field_0').style.display='none';
-	document.getElementById('field_3').style.display='block';
-	music.pause();
-	yay.play();
+	killCoeff = 0;
+	document.getElementById('field_0').style.display = 'none';
+	document.getElementById('field_3').style.display = 'block';
+	bgMus.pause();
+	succMus.play();
 }
 
-// Level counter
-function lvlcount() {
-	switch (token) {
-		case 1:
-		lvl.value = 'Sloppy';
-		break;
-		case 2:
-		lvl.value = 'Irish';
-		break;
-		case 3:
-		lvl.value = 'Spanish';
-		break;
-		case 4:
-		lvl.value = 'Polish';
-		break;
-		case 5:
-		lvl.value = 'Gibberish';
-		break;
-		default:
-		lvl.value = 'Model Mid';
-		break;
+// Failure function
+function fail() {
+	killCoeff = 0;
+	document.getElementById('field_0').style.display = 'none';
+	document.getElementById('field_1').style.display = 'block';
+	bgMus.pause();
+	failMus.play();
+}
+
+// Pause functions
+function menu() {
+	bgMus.pause();
+	document.getElementById('field_2').style.display = 'block';
+	paused = !paused;
+	draw();
+}
+
+function resume() {
+	bgMus.play();
+	document.getElementById('field_2').style.display = 'none';
+	paused = !paused;
+	draw();
+}
+
+// Reload function
+function restart() location.reload();
+
+// Drawer function
+function draw() {
+	if (!paused) {
+		document.getElementById('field_2').style.display = 'none';
+		window.requestAnimationFrame(loop);
 	}
-	loop;
-}
-
-// Fullscreen mode
-function full() {
-	canvas.requestFullscreen();
 }
 
 // Main script
-const loop = function () {
-	
-	if (frameCount == 1) {
-		if (paused == false) {
-			music.play();
-		} else {
-			music.pause();
-		}
-	}
-	if (frameCount == 9) {
-		success();
+function loop() {
+	// Event checks
+	if (controller.esc) {
+		if (paused) resume();
+		else if (!paused) menu();
 	}
 	
+	if (controller.up && !player.jumping) {
+		player.Vy -= 30;
+		player.jumping = !player.jumping;
+		jmpSound.play();
+		setTimeout(function() player.jumping = !player.jumping;, 500);
+	}
+	
+	// Frame check
 	switch (frameCount) {
 		case 1:
-		bg = document.getElementById("bg1");
-		loc.value = "ACME";
+			bg = document.getElementById('bg1');
+			loc.value = 'ACME';
 		break;
 		case 2:
-		bg = document.getElementById("bg2");
-		loc.value = "Fed House";
+			bg = document.getElementById('bg2');
+			loc.value = 'Fed House';
 		break;
 		case 3:
-		bg = document.getElementById("bg3");
-		loc.value = "Dillo's";
-		break;	
+			bg = document.getElementById('bg3');
+			loc.value = 'Dillo\'s';
+		break;
 		case 4:
-		bg = document.getElementById("bg4");
-		loc.value = "Gate 1";
+			bg = document.getElementById('bg4');
+			loc.value = 'Gate 1';
 		break;
 		case 5:
-		bg = document.getElementById("bg5");
-		loc.value = "Red Beach";
+			bg = document.getElementById('bg5');
+			loc.value = 'Red Beach';
 		break;
 		case 6:
-		bg = document.getElementById("bg6");
-		loc.value = "4-0";
+			bg = document.getElementById('bg6');
+			loc.value = '4-0';
 		break;
 		case 7:
-		bg = document.getElementById("bg7");
-		loc.value = "4-4 P-Way";
+			bg = document.getElementById('bg7');
+			loc.value = '4-4 P-Way';
 		break;
-		default:
-		bg = document.getElementById("bg8");
-		loc.value = "Wardroom";
+		case 8:
+			bg = document.getElementById('bg8');
+			loc.value = 'Wardroom';
+		break;
+		default: success();
 		break;
 	}
-
-	if (controller.up && square.jumping == false) {
-		square.yVelocity -= 30;
-		boing.play();
-		square.jumping = true;
-		square.coltok = false;
-		setTimeout(function(){
-				square.jumping = false ;}, 500);
-	}
-  
-	square.xVelocity = (4 + (frameCount/2))*kill;
-	square.yVelocity += 1.5;
 	
-	if (notsquare.xVelocity > 0) {
-		notsquare.xVelocity = (4 + (token + 1))*kill;
-	} else {
-		notsquare.xVelocity = -1*(4 + (token + 1))*kill;
-	}
+	// Element Kinematics
+	player.Vx = killCoeff*(4 + (frameCount/2));
+	player.Vy += 1.5; // *** ??? ***
+	player.x += player.Vx;
+	player.y += player.Vy;
 	
-	notsquare.yVelocity += 1.5;
+	opponent.Vx = killCoeff*(5 + lvlToken);
+	if (opponent.x < -20 || opponent.x > 1390) opponent.Vx *= -1;
+	opponent.x += opponent.Vx;
 	
-	square.x += square.xVelocity;
-	square.y += square.yVelocity;
-	
-	square.xVelocity *= 0.9;
-	square.yVelocity *= 0.9;
-	
-	notsquare.yVelocity *= 0.9;
-	
-	notsquare.x += notsquare.xVelocity;
-	notsquare.y += notsquare.yVelocity;
-  
-	// Static Collision
-	for (let j=0; j < obXCoors.length; j++) {
-		Cols[j] = Math.abs(obXCoors[j] - square.x);
-		
-		if (Cols[j] < 30 && square.coltok == true) { //square.jumping == false
-			oof.play();
-			oof.volume = 1.0;
-			square.x = -20;
-			square.y -= 586 - 16 - 32;
-			demerits += 50;
-			token += 1;
-			dcount.value = parseInt(dcount.value) + 10*token;
-			//dcount.value += 10*token;
-			lvlcount();
-			if (parseInt(dcount.value) > 100) {
-				over();
-			} else {
-				document.getElementById('field_0').style.display='block' ;
-				setTimeout(function(){
-					document.getElementById('field_0').style.display='none' ;}, 1000);
-			}
-		}
-	};
-	
-	// Dynamic Collision
-	obCol = Math.abs(notsquare.x - square.x);
-	
-	if (obCol < 30 && square.coltok == true) { //square.jumping == false
-		oof.play();
-		oof.volume = 1.0;
-		square.x = -20;
-		square.y -= 586 - 16 - 32;
-		demerits += 50;
-		token += 1;
-		dcount.value = parseInt(dcount.value) + 10*token;
-		//dcount.value += 10*token;
-		lvlcount();
-		if (parseInt(dcount.value) > 100) {
-			over();
-		} else {
-			document.getElementById('field_0').style.display='block' ;
-			setTimeout(function(){
-				document.getElementById('field_0').style.display='none' ;}, 1000);
-		}
-	}
-
-	// Solid floor
-	if (square.y > 586 - 16 - 125) {
-		// square.jumping = false;
-		square.coltok = true;
-		square.y = 586 - 16 - 125;
-		square.yVelocity = 0;
-	}
-	
-	if (notsquare.y > 586 - 16 - 125) {
-		notsquare.jumping = false;
-		notsquare.y = 586 - 16 - 125;
-		notsquare.yVelocity = 0;
-	}
-
-	// Continuous screen
-	if (square.x < -20) {
-		square.x = 1400;
-	} else if (square.x > 1400) {
-		square.x = -20;
+	// Frame Physics
+	if (player.x < -20) player.x = 1400;
+	else if (player.x > 1400) {
+		player.x = -20;
 		nextFrame();
 	}
 	
-	// Obstacle roaming
-	if (notsquare.x < -20 || notsquare.x > 1390) {
-		notsquare.xVelocity *= -1;
+	if (player.y > 586 - 16 - 125) {
+		player.y = 586 - 16 - 125;
+		player.Vy = 0;
 	}
-  
-	// Frame backgd
-	context.drawImage(bg, 0, 0, 1400, 600); // x, y, width, height
-
-	// Player element
-	context.beginPath();
-	context.drawImage(pic, square.x, square.y, square.width, square.height);
-	context.fill();
-
-	// Obstacle generator
-	context.beginPath();
-	context.drawImage(pic2, notsquare.x, notsquare.y, notsquare.width, notsquare.height);
-	context.fill();
 	
-	// Obstacle generator
-
-	obXCoors.forEach((obXCoor) => {
-		context.beginPath();
-		context.drawImage(pic3, obXCoor, 586 - 16 - 125, 125, 125); 
-		context.closePath();
-		context.fill();
-	})
-
-	// Ground element
-	context.strokeStyle = "#000000";
+	// Static Collision
+	for (let i = 0; i < statXCoors.length; i ++) {
+		if (player.x < statXCoors[i] + squareDim &&
+		player.x + player.width > statXCoors[i] &&
+		player.y < 445 + squareDim &&
+		player.height + player.y > 445) collide();
+	}
+	
+	// Dynamic Collision
+	if (player.x < opponent.x + opponent.width &&
+    player.x + player.width > opponent.x &&
+    player.y < opponent.y + opponent.height &&
+    player.height + player.y > opponent.y) collide();
+	
+	// Frame Drawing
+	context.drawImage(bg, 0, 0, width, height);
+	
+	// Ground Drawing
+	context.strokestyle = '#000000';
 	context.lineWidth = 30;
 	context.beginPath();
 	context.moveTo(0, 585);
 	context.lineTo(1400, 585);
 	context.stroke();
+	
+	// Player Drawing
+	context.beginPath();
+	context.drawImage(img1, player.x, player.y, player.width, player.height);
+	context.fill();
+	
+	// Dynamic Obstacle Drawing
+	context.beginPath();
+	context.drawImage(img2, opponent.x, opponent.y, opponent.width, opponent.height);
+	context.fill();
+	
+	// Static Obstacles Drawing
+	statXCoors.forEach((statXCoor) => {
+		context.beginPath();
+		context.drawImage(img3, statXCoor, 586 - 16 - 125, 125, 125);
+		context.closePath();
+		context.fill();
+	})
+	
 	draw();
 }
 
-// Drawer
-function draw() {
-	if (paused == false) {
-		document.getElementById('field_2').style.display='none';
-		window.requestAnimationFrame(loop);
-	}
-}
-
-window.addEventListener("keydown", controller.keyListener);
-window.addEventListener("keyup", controller.keyListener);
+window.addEventListener('keydown', controller.keyListener);
+window.addEventListener('keyup', controller.keyListener);
 window.requestAnimationFrame(loop);
 
-// All credits to William Andriamihaja, with a little help from StackOverflow.com
+// Program coded by William Andriamihaja
